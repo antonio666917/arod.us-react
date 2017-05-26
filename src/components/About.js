e77ccb5608fcc68cbfd772a5003824407a55e132
Antonio Rodríguez <antoniwan@gmail.com>
2017-05-25 20:19:10 -0400

    Initialize the About component

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,43 @@ import './About.css';
 
 function About() {
   return (
-    <div className="wrapper">
-      <h1>About</h1>
+    <div className="about wrapper">
+      <h1>About Me</h1>
+      <p>Building things brings me immense satisfaction.</p>
+
+      <p>I have over ten years of professional experience leading teams, mentoring interns, as a software engineer, as a project manager, as an external consultant, as a digital marketing technologist and hacking my way through any technical challenge that showed its beautiful face. Check out my <a href="https://www.linkedin.com/in/antoniwan" target="_blank" rel="noopener">LinkedIn profile</a> or <a href="/resume">resumé</a>. Two out of two of the companies that I've built, or helped built, have been acquired. Boom!</p>
+
+      <p>I currently live in Miami, Florida and am happily married with an awesome 12-year-old kiddo. English is my second language and I may or may not be a horrible writer.</p>
+
+      <h3>Software engineering</h3>
+
+      <p>My computer science academic background permits me to jump around languages, frameworks, and design patterns with relatively less punishment. I'm eternally grateful to my college professors and my father for instilling what has become an addiction for learning. In our field of work it has to be this way. Continual re-education must be a serious commitment. I've been in a management position (still am) for the last 5 years so I'm always being forced to play catch-up on the engineering skills. Assisting people all day <strong>!=</strong> hacking all day.</p>
+
+      <p>For most projects, I will default to working with the LAMP or MEAN stacks but my professional proficiency is not limited to these.</p>
+
+      <p>I absolutely love git, scaffolding tools, sass, JavaScript, the BEM methodology, style guides, how accessible AI is now, the "Ballmer Peak", good documentation and this year I'm focusing on falling for conversational interfaces, Angular, React, React Native, Swift, WebPack, TypeScript and artificial intelligence PaaS.</p>
+
+      <p>One regret I have, one which I hope to remedy soon, is my lack of work on public open source projects. I feel shitty about not contributing back!</p>
+
+      <h3>Agency life</h3>
+
+      <p>Working out of a marketing agency lets me build and deploy things insanely fast and I love this! My teams ship out hundred of projects each year, each with variable scopes of work and duration. Our top deliverables are websites, promotional apps, chatbots and 'custom' tools.</p>
+
+      <p>My favorite project at the moment is our MINE data platform. It's a badass analytics, insights and reporting tool that consolidates data from hundreds of media campaigns (Talend -> BigQuery -> custom management tools -> Tableau) from over 36 data sources of media, social, traffic, placement and revenue. I consider this the heart of our <a href="https://m8agency.com" target="_blank" rel="noopener noreferrer">agency</a>.</p>
+
+      <h3>Something extra</h3>
+
+      <ul>
+        <li>I can effortlessly eat two large pizzas by myself.</li>
+        <li>Before college I was an athlete! Black belts in Tae-Kwon-Do and Shotokan. Also achieved level 5 in gymnastics!</li>
+        <li>I achieved some certifications while attending high-school (CCNA, MSCE, Net+ and A+) and worked as a network specialist at the University of Puerto Rico for several months.</li>
+        <li>When I moved to Miami I founded the "<a href="https://www.facebook.com/groups/1395203817368867/" target="_blank" rel="noopener">Below-Amateur Athletes Club</a>" because we couldn't find  people to play basketball with. <em>I love playing basketball!</em></li>
+        <li>While working as a network specialist at the University of Puerto Rico's main precinct, a friend and I challenged the Dean of Technology to let us rebuild the aging website... he accepted and I quickly became the Lead Web Developer for the whole precinct! I built a suite of web-based student services: enrollment, tuition payments, online grades, schedules, academic progress evaluations, and other services. These web services are still in use to this date, hopefully with updates!</li>
+        <li>During "the big 2010 student strike", I built the first secure electronic referendum system employed by any governmental or educational institution in Puerto Rico. It was called the "Sistema de Consulta al Estudiante".</li>
+        <li>Started our first business with a best friend, Jorge Suarez (he's now the head of product for a fancy new startup called Hyp3r) and the experience was just mind-blowing. It was a creative web development studio called "Kolapse Interactive". Business was good. Eventually, a digital marketing agency from Miami acquired Kolapse.</li>
+        <li>Got addicted to hackathons after attending the PayPal BattleHack Miami 2014 and winning a <a href="https://scontent.xx.fbcdn.net/v/t1.0-9/1609626_10152222065329820_2114747425_n.jpg?oh=2d7c930b55cace33e8c91051b7255d2e&oe=596450B6" target="_blank" rel="noopener">PlayStation 4</a> as a 2nd place prize. I get to do what I love AND WIN CRAZY PRIZES? OH MY!</li>
+        <li>My wife and I met on twitter.</li>
+      </ul>
     </div>
   )
 }
