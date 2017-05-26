0efb710cf623910a85287dfafdb53cceb5cca9ea
Antonio Rodríguez <antoniwan@gmail.com>
2017-05-25 20:28:34 -0400

    Streamlined wrapper classes

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,14 @@ import './Nav.css';
 
 function Nav() {
   return (
-    <div className="nav-wrapper">
+    <div className="wrapper nav-wrapper">
       <NavLink exact className="nav-logo" activeClassName="inactive" to="/">antoniorodriguez.us</NavLink>
       <ul className="nav">
         <li>
           <NavLink exact activeClassName="active" to="/">Home</NavLink>
         </li>
         <li>
-          <NavLink activeClassName="active" to="/about">About</NavLink>
+          <NavLink activeClassName="active" to="/about">About Me</NavLink>
         </li>
       </ul>
     </div>
