416c311e3d79de7baae7231305318d6561acd930
Antonio Rodríguez <antoniwan@gmail.com>
2017-05-25 20:18:55 -0400

    Initialize the footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="wrapper">
+
+        <div className="footer_credits">
+          <h5>Antonio Rodríguez</h5>
+          <h6>software engineering &#9737; web development &#9991; digital marketing &#9862; technology</h6>
+        </div>
+
+        <div className="footer_social">
+          social media links go here
+        </div>
+
+      </div>
+    </footer>
+  )
+}
+export default Footer;
