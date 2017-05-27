1cdbe0402c18d0878b5c39b86faed4569b1cb298
Antonio Rodríguez <antoniwan@gmail.com>
2017-05-26 21:07:14 -0400

    Lots of copy changes

--- src/components/About.js
+++ src/components/About.js
@@ -6,28 +6,20 @@ function About() {
   return (
     <div className="about wrapper">
       <h1>About Me</h1>
-      <p>Building things brings me immense satisfaction. I have over ten years of professional experience leading teams, mentoring interns, as a software engineer, as a project manager, as an external consultant, as a digital marketing technologist and hacking my way through any technical challenge that showed its beautiful face. Check out my <a href="https://www.linkedin.com/in/antoniwan" target="_blank" rel="noopener noreferrer">LinkedIn profile</a>. Two out of two of the companies that I've built, or helped built, have been acquired. Boom!</p>
-
+      <p>Building software brings me immense satisfaction.  I own experience leading teams, mentoring interns, as a software engineer, as a project manager, as an external consultant, as a digital marketing technologist and hacking my way through any technical challenge that showed its beautiful face. Check out my <a href="https://www.linkedin.com/in/antoniwan" target="_blank" rel="noopener noreferrer">LinkedIn profile</a>. Two out of two of the companies that I've built, or helped built, have been acquired. Boom!</p>
       <p>I'm happily married and currently living in Miami. English is my second language and I may or may not be a horrible writer.</p>
 
       <h3>Technology</h3>
-
-      <p>I'm eternally grateful to my college professors, and father, for instilling what has become an addiction for learning. In our field of work, it has to be this way because continuous education must be a serious commitment. My academic background, mainly in computer science, permits me to jump around languages, frameworks, and design patterns with, what I think, is relative ease. And I the end of the day, I don't want to marry myself with any specific technology; <span title="Sometimes we learn the most by making the wrong decisions 😭">one must choose the correct tool for each situation</span>.</p>
-
+      <p>I'm eternally grateful to my college professors, and father, for instilling what has become an addiction for learning. In our field of work, it has to be this way because continuous education must be a serious commitment. My academic background, mainly in computer science, permits me to jump around languages, frameworks, and design patterns with, what I think, is relative ease. And in the end of the day, I don't want to marry myself with any specific technology; <span title="Sometimes we learn the most by making the wrong decisions 😭">one must choose the correct tool for each situation</span>.</p>
       <p>For most projects, I will default to working with the LAMP or MEAN stacks, but my 'production' proficiency is not limited to these. I'm always encouraging my teams to try the next thing, make proof-of-concepts, and share the learning with the rest of the team. We feel this is metaphorically similar to how Batman maintains his belt tools <span role="img" aria-label="Flex muscles">💪🏼</span>.</p>
-
       <p>I absolutely LOVE git, zsh, scaffolding tools like Yeoman, Sass, JavaScript, the BEM methodology, linters, how accessible AI is nowadays, the "Ballmer Peak" and concise documentation. This year I'm focusing my learning efforts on conversational interfaces, ES6, Angular, React, React Native, WebPack, TypeScript, NodeJS and artificial intelligence PaaS.</p>
-
       <p>A regret I have, one which I hope to remedy soon, is my lack of work on open source projects. I feel shitty about not contributing back!</p>
 
       <h3>#AdLife</h3>
-
       <p>Working out of a marketing agency lets me build and deploy things insanely fast and I love this! My teams ship out hundred of projects each year, each with variable scopes of work and duration. Our top deliverables are websites, promotional apps, chatbots and 'custom' tools.</p>
-
       <p>My favorite project at the moment is our MINE data platform. It's a badass analytics, insights and reporting tool that consolidates data from hundreds of media campaigns (Talend -> BigQuery -> custom management tools -> Tableau) from over 36 data sources of media, social, traffic, placement, and revenue. I consider this the heart of our <a href="https://m8agency.com" target="_blank" rel="noopener noreferrer">agency</a>.</p>
 
       <h3>Something extra</h3>
-
       <ul>
         <li>We have a 12-year old boy, who is a TOTAL BADASS!</li>
         <li>I can effortlessly eat two large pizzas by myself.</li>
@@ -39,7 +31,7 @@ function About() {
         <li>I was part of a nu-metal band from Puerto Rico, called <a href="https://www.google.com/search?q=mabuyan&nfpr=1&source=lnms&sa=X&ved=0ahUKEwik_9P4x43UAhVM5yYKHUuTC-QQ_AUICSgA&biw=1344&bih=1032&dpr=2" target="_blank" rel="noopener noreferrer">Mabuyan</a>.</li>
         <li>Started my first business with a great friend, Jorge Suarez (he's now the head of product for a fancy new startup called Hyp3r) and the experience was just mind-blowing. It was a creative web development studio called "Kolapse Interactive". Business was good. Eventually, a digital marketing agency from Miami acquired Kolapse.</li>
         <li>Got addicted to hackathons after attending the PayPal BattleHack Miami 2014 and winning a <a href="https://scontent.xx.fbcdn.net/v/t1.0-9/1609626_10152222065329820_2114747425_n.jpg?oh=2d7c930b55cace33e8c91051b7255d2e&oe=596450B6" target="_blank" rel="noopener noreferrer">PlayStation 4</a> as a 2nd place prize. I get to do what I love AND WIN CRAZY PRIZES? OH MY!</li>
-        <li>My wife and I met on twitter.</li>
+        <li>My wife and I met on Twitter.</li>
       </ul>
     </div>
   )
